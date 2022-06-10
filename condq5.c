9e6a338e37e7a7c6a51ba12f917180f3e86afbed
Priyanka-Marandi <priyankamarandi0303@gmail.com>
2022-06-10 12:18:58 +0530

    init

---
+++ condq5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int m;
+    printf("Enter the value of m:");
+    scanf("%d",&m);
+    if(m>0)
+    printf("the value of n = 1");
+    else if(m==0)
+    printf("the value of n = 0");
+    else
+    printf("the value of n = -1");
+    return 0;
+}
