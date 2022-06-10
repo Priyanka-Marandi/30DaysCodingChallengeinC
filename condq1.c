8da7946570f7f8ee65c1745cd2573d5583171537
Priyanka-Marandi <priyankamarandi0303@gmail.com>
2022-06-10 11:55:46 +0530

    init

---
+++ condq1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("Enter the value of b:");
+    scanf("%d",&b);
+    if(a==b)
+    printf("%d == %d ",a,b);
+    return 0;
+}
